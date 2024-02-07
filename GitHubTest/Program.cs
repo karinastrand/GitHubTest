6e737fedf94f80f46ad87d0c23d92da0426eb845
Karin Åstrand <karinastrand@yahoo.se>
2024-02-07 11:56:17 +0100

    #2 added test-1 branch

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -1,2 +1,4 @@
-﻿//This code added from Master branch
+﻿//This code is added from Master branch
 Console.WriteLine("This code is for testing Master Branch");
+//Thid code is added from test-1 branch
+Console.WriteLine("Test-1 branch is added");
