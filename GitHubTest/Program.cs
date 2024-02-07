b5ee100bf13a1914aa1f0330e8fc99bbd2ee9745
Karin Åstrand <karinastrand@yahoo.se>
2024-02-07 11:47:55 +0100

    #1 added some code

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//This code added from Master branch
+Console.WriteLine("This code is for testing Master Branch");
