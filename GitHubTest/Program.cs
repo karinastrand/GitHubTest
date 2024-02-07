712c97f4afb4dc96e2d8c066afff782ceeddb127
Karin Åstrand <karinastrand@yahoo.se>
2024-02-07 12:42:15 +0100

    #4 updating test-1

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("This code is for testing Master Branch");
 //Thid code is added from test-1 branch
 Console.WriteLine("Test-1 branch is added");
+//This will give a conflict 
+Console.WriteLine("Not updated, will take test-2 out of master");
